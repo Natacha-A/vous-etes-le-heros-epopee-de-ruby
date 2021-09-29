f4f734e56598d4fa57f2b98a7c5e1f573edda8ef
Natacha-A <90852900+Natacha-A@users.noreply.github.com>
2021-09-29 16:22:22 -0400

    ajoutezJavaScript
    ajoutez objet js chapterObj et le premier objet et ces valeurs (Sous-titre, text et l'image)

---
+++ script.js
@@ -0,0 +1,6 @@
+let chaptersObj = {
+    le_reveil: "premierchapitre",
+    subtitle: "L'aventure Commence!!",
+    text: "Tu te reveille au son de quelqu'un qui cogne à la porte. C'est Fjorm, elle vous supplie de l'aider contre le royaume de Muspel qui menace de détruire son royaume Nifl",
+    img: "royaume_askr.png"
+}
