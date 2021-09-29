069d145710de8d3971f0ce9ccad49765415668b1
Natacha-A <90852900+Natacha-A@users.noreply.github.com>
2021-09-29 16:55:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let chaptersObj = {
-    le_reveil = { 
-        le_reveil: premierchapitre,  
+    le_reveil = {  
         subtitle: "L'aventure Commence!!",
         text: "Tu te reveille au son de quelqu'un qui cogne à la porte. C'est Fjorm, elle vous supplie de l'aider contre le royaume de Muspel qui menace de détruire son royaume Nifl",
         img: "royaume_askr.png"
